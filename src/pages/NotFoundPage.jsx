d75d14dd561129f0e8f4f02b8a3fba0e154c665b
arabengineersweb <arabengineersweb@gmail.com>
2025-10-26 11:03:17 +0530

    Fixed 404 error page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import NotFound from '../components/NotFound'
 
 export default function NotFoundPage({ lang, setLang }) {
-  return (
-    <>
-      <Header lang={lang} setLang={setLang} />
-      <main>
-        <NotFound lang={lang} />
-      </main>
-      <Footer lang={lang} />
-    </>
-  )
+  return <NotFound lang={lang} />
 }
 
